0f2b3fc3c2531ab291f290e94ec08a9cbfd8662a
Debora Schuch da Rosa <starambients@gmail.com>
2022-01-04 15:56:40 +0100

    Formulario de registro sem link a php dessa vez... e com funcoes de validacao em JavaScript

--- Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 06_ HTML e CSS - Forms, Flexbox e Responsivo/01_ HTML & CSS - Forms/FormularioDeRegistro/script.js
+++ Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 06_ HTML e CSS - Forms, Flexbox e Responsivo/01_ HTML & CSS - Forms/FormularioDeRegistro/script.js
@@ -8,3 +8,23 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+
+// check if name only contains letters and whitespace
+const name = document.getElementById("name");
+if (!preg_match("/^[a-zA-Z-' ]*$/", name)) {
+  const nameErr = "Only letters and white space allowed";
+}
+
+// check if e-mail address is well-formed
+const email = document.getElementById("email");
+if (!filter_var(email, FILTER_VALIDATE_EMAIL)) {
+  const emailErr = "Invalid email format";
+}
+
+// check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+const website = document.getElementById("website");
+if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",website)) {
+  const websiteErr = "Invalid URL";
+}
+
